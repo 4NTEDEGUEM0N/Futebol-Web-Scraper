dd53270c623d07e6d79beb63e58a0ffd06674246
4NTEDEGUEM0N <solbrilhan@gmail.com>
2024-08-06 15:41:20 -0300

    Usando openpyxl no lugar do pandas

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from datetime import datetime
-from math import isnan
+from openpyxl import load_workbook
 
 times = {
     "São Paulo": "sao-paulo",
@@ -37,19 +36,20 @@ path_planilha = "/home/thiago/Área de trabalho/palpitometro.xlsx"
 
 def verificar_partidas_planilha(path_planilha):
     agora = datetime.now().strftime("%Y-%m-%d")
-    df = pd.read_excel(path_planilha)
+    wb = load_workbook(filename=path_planilha)
+    sheet = wb['Palpi']
     partidas_atualizar = []
-    for index, row in df.iterrows():
-        row_list = row.tolist()
-        if not isnan(row_list[0]):
-            if isinstance(row_list[1], datetime):
-                data_partida = row_list[1].strftime("%Y-%m-%d")
-                if agora >= data_partida and isnan(row_list[4]):
-                    time_casa = row_list[2]
-                    time_fora = row_list[8]
-                    placar = [4, 6]
+    for index, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+        if not None:
+            if isinstance(row[1], datetime):
+                data_partida = row[1].strftime("%Y-%m-%d")
+                if agora >= data_partida and row[4] is None:
+                    time_casa = row[2]
+                    time_fora = row[8]
+                    placar = [row[4], row[6]]
                     partidas_atualizar.append([index, time_casa, time_fora, placar, data_partida])
-    return partidas_atualizar, df
+
+    return partidas_atualizar, wb, sheet
 
 
 def buscar_resultados(partidas_atualizar):
@@ -75,7 +75,7 @@ def buscar_resultados(partidas_atualizar):
     return partidas_resultado
 
 
-def atualizar_partidas(partidas_atualizar, partidas_resultado, df):
+def atualizar_partidas(partidas_atualizar, partidas_resultado, sheet):
     if not partidas_resultado:
         return "Nenhuma resultado encontrado"
     elif not partidas_atualizar:
@@ -83,16 +83,19 @@ def atualizar_partidas(partidas_atualizar, partidas_resultado, df):
     else:
         for chave, resultado in enumerate(partidas_resultado):
             if resultado[4] == partidas_atualizar[chave][4]:
-                df.iloc[partidas_atualizar[chave][0], 4] = float(resultado[3][0])
-                df.iloc[partidas_atualizar[chave][0], 6] = float(resultado[3][1])
-    return df
+                sheet.cell(row=partidas_atualizar[chave][0], column=5).value = float(resultado[3][0])
+                sheet.cell(row=partidas_atualizar[chave][0], column=7).value = float(resultado[3][1])
+    return sheet
 
 
-partidas_atualizar, df = verificar_partidas_planilha(path_planilha)
+partidas_atualizar, wb, sheet = verificar_partidas_planilha(path_planilha)
 partidas_resultado = buscar_resultados(partidas_atualizar)
-df_atualizado = atualizar_partidas(partidas_atualizar, partidas_resultado, df)
+sheet_atualizada = atualizar_partidas(partidas_atualizar, partidas_resultado, sheet)
 
 print(partidas_atualizar)
 print(partidas_resultado)
 for partida in partidas_atualizar:
-    print(df_atualizado.iloc[partida[0]].tolist()[2:9])
+    linha_valores = [cell.value for cell in sheet[partida[0]]]
+    print(linha_valores)
+
+wb.save("/home/thiago/Área de trabalho/palpitometro_atualizado.xlsx")
